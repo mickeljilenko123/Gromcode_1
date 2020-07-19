8b09c6854c325d2ec01b2c2aaaf6ad37305b7c5c
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-19 22:57:50 +0300

    Js 6.3/2

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const numbers = [2, 26, 5, 44];
 function squareArray(arr) {
     let squareResult = [];
     for (let i = 0; i < arr.length; i++) {
-        squareResult.push(arr[i] *= arr[i]);
-    }
-    if (!Array.isArray(numbers)) {
-        return null;
+        squareResult.push(arr[i] * arr[i]);
+        if (!Array.isArray(arr)) {
+            return null;
+        }
     }
     return squareResult;
 }
-console.log(squareArray(numbers));
+
+const squareArr = squareArray(numbers);
+
+console.log(squareArr);
