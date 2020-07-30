622dc1d25ba1b17fabf94b31c6a59b9b94f9b1df
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-30 18:34:30 +0300

    js 7.2

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const sortDesc = numbers => {
-    function sompare(a, b) {
-        if (a < b) {
-            return 1;
-        }
-        return -1;
-    }
-    numbers.sort(sompare);
-
-    return numbers;
+    const copy = numbers.slice();
+    return copy.sort((a, b) => b - a);
 };
 
 const arr = [2, 57, 7, 67, 90, 6, 14];
 
 console.log(sortDesc(arr));
+console.log(arr);
