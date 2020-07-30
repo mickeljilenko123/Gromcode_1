ae459d467a8175ab364742c08310f4d0259bcbcb
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-30 22:14:29 +0300

    js 7.5

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-const squareArray = numbers => {
-    if (!Array.isArray(numbers)) {
+// const squareArray = numbers => {
+//     if (!Array.isArray(numbers)) {
+//         return null;
+//     }
+//     let specialNumbers = [];
+
+//     numbers.forEach(num => {
+//         num *= num;
+//         specialNumbers.push(num);
+//     });
+
+//     // for (let i = 0; i < numbers.length; i++) {
+//     //     numbers[i] *= numbers[i];
+//     //     specialNumbers.push(numbers[i]);
+//     // }
+//     return specialNumbers;
+// };
+// let arr = [1, 4, 6, 8, 9];
+
+// console.log(squareArray(arr));
+
+
+
+const arr = [1, 4, 6, 8, 9];
+const squareArray = arr.map(ar => {
+    if (!Array.isArray(arr)) {
         return null;
     }
-    let specialNumbers = [];
+    return ar * ar;
+});
 
-    numbers.forEach(num => {
-        num *= num;
-        specialNumbers.push(num);
-    });
-
-    // for (let i = 0; i < numbers.length; i++) {
-    //     numbers[i] *= numbers[i];
-    //     specialNumbers.push(numbers[i]);
-    // }
-    return specialNumbers;
-};
-let arr = [1, 4, 6, 8, 9];
-
-console.log(squareArray(arr));
+console.log(squareArray);
