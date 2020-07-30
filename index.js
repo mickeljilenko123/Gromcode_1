d49435352e11bb4df59fb8ebd976f76159452d48
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-30 22:18:02 +0300

    js 7.5

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-// const squareArray = numbers => {
-//     if (!Array.isArray(numbers)) {
-//         return null;
-//     }
-//     let specialNumbers = [];
-
-//     numbers.forEach(num => {
-//         num *= num;
-//         specialNumbers.push(num);
-//     });
-
-//     // for (let i = 0; i < numbers.length; i++) {
-//     //     numbers[i] *= numbers[i];
-//     //     specialNumbers.push(numbers[i]);
-//     // }
-//     return specialNumbers;
-// };
-// let arr = [1, 4, 6, 8, 9];
-
-// console.log(squareArray(arr));
-
-
-
-const arr = [1, 4, 6, 8, 9];
-const squareArray = arr.map(ar => {
-    if (!Array.isArray(arr)) {
+const squareArray = numbers => {
+    if (!Array.isArray(numbers)) {
         return null;
     }
-    return ar * ar;
-});
+    let specialNumbers = [];
 
-console.log(squareArray);
+    numbers.map(num => {
+        num *= num;
+        specialNumbers.push(num);
+    });
+
+    // for (let i = 0; i < numbers.length; i++) {
+    //     numbers[i] *= numbers[i];
+    //     specialNumbers.push(numbers[i]);
+    // }
+    return specialNumbers;
+};
+let arr = [1, 4, 6, 8, 9];
+
+console.log(squareArray(arr));
+
+
+
+// const arr = [1, 4, 6, 8, 9];
+// const squareArray = arr.map(ar => {
+//     if (!Array.isArray(arr)) {
+//         return null;
+//     }
+//     return ar * ar;
+// });
+
+// console.log(squareArray);
