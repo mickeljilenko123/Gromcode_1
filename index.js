a0393fd0468b21c0c2ff985b861acdd0c74ca9ec
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-02 00:41:41 +0300

    js 6.6

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 
 
 const arr = [1, 2, 4, 5];
+const squareArray = function(arr) {
 
-for (let i = 0; i < arr.length; i += 1) {
-    arr[i] *= arr[i];
+    for (let i = 0; i < arr.length; i += 1) {
+        arr[i] *= arr[i];
+
+    }
+    return arr;
 
 }
-console.log(arr);
+console.log(squareArray(arr));
