46e92e3615f5e1a1d58f5b50ad2cef81fc4313a5
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-02 15:20:36 +0300

    js 8.3

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 'use strict'
 
-const user = {
-    name: 'John Doe',
-    age: 17,
-    interest: 'footbool',
-}
-const concatProps = user => {
-    let arr = [];
-    for (const key in user) {
-        arr.push(user[key]);
-        // arr = arr.concat(obj[key]);
-    }
-    return arr;
-}
-console.log(concatProps(user));
+const transaction = {
+    currency: 'USD',
+    value: 170,
+    operation: 'sale',
+    agent: {
+        country: 'Ukraine',
+        company: 'NYSE'
+    },
+    'operation time': 1584029990001
+};
+
+/* выведите в консоль значение свойства 'currency' с помощью точки */
+console.log((transaction.currency));
+
+/* выведите в консоль значение свойства 'value' с помощью квадратных скобок */
+console.log(transaction['value']);
+
+const key = 'operation';
+/* выведите в консоль значение свойства, название которого находится в переменной key - используйте квадратные скобки */
+console.log(['operation']);
+/* выведите в консоль значение свойства 'company' используя квадратные скобки */
+console.log(transaction.agent['company']);
+/* выведите в консоль значение свойства 'country' используя точку */
+console.log(transaction.agent.country);
+/* выведите в консоль значение свойства 'operation time' используя квадратные скобки */
+console.log(transaction['operation time']);
