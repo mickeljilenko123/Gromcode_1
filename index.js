7bfd3f71907aa89832271b8145cf0b4895441b66
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-02 17:26:32 +0300

    js 8.5

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
 'use strict'
-
-const user = {
-    name: 'Tom',
-    age: 17,
+const array = [1, 4, 7, 'Polya']
+const getKeys = array => {
+    let obj = {};
+    array.forEach(key => {
+        obj[key] = key
+    });
+    return obj;
 };
-
-user.hobby = 'football';
-user['married'] = false;
-user['favorite music'] = 'rock';
-user.address = {};
-user.address.country = 'Ukraine';
-user.address.building = 17;
-
-console.log(user);
-
-/* присвойте объекту свойство hobby со значением 'football' с помощью точки */
-
-const anotherKey = 'married';
-/* название свойства хранится в переменной anotherKey. 
-Присвойте объекту такое свойство со значением false */
-
-/* присвойте объекту свойство 'favorite music' со значением 'rock' */
-
-/* присвойте объекту свойство 'address', которое будет пустым объкетом */
-
-/* присвойте объекту в 'address' свойство 'country', со значением 'Ukraine' */
-
-/* присвойте объекту в 'address' свойство 'building', со значением 17 */
-
-/* выведите в консоль объект user */
+console.log(getKeys(array));
