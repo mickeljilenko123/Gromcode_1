2421a7928be03c21197e7331dce4705b7acd59fe
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-03 15:29:27 +0300

    js 8.5

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 
+
 const addPropertyV1 = (obj, key, value) => {
     obj[key] = value;
     return obj;
@@ -8,22 +9,30 @@ const addPropertyV1 = (obj, key, value) => {
 
 const addPropertyV2 = (obj, key, value) => {
     const result = Object.assign(obj, {
-        [key]: value });
+        [key]: value
+    });
     return result;
 };
 
 const addPropertyV3 = (obj, key, value) => {
     const result = Object.assign({}, obj, {
-        [key]: value });
+        [key]: value
+    });
     return result;
 };
 
 const addPropertyV4 = (obj, key, value) => {
     const result = Object.assign({...obj }, {
-        [key]: value });
+        [key]: value
+    });
     return result;
 };
 
 const user = {
-    name: 'Tom',
+    lastName: 'King',
 };
+
+console.log(addPropertyV1(user, 'firstName', 'Stephen'));
+console.log(addPropertyV2(user, 'firstName', 'Stephen'));
+console.log(addPropertyV3(user, 'firstName', 'Stephen'));
+console.log(addPropertyV4(user, 'firstName', 'Stephen'));
