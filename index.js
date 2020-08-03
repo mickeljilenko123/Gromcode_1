67dfff9c282de4bd53cfb8ecbca5b613bc986fe3
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-03 21:31:40 +0300

    js

--- index.js
+++ index.js
@@ -1,46 +1,22 @@
-//1. input: obj and array
-//2. output: obj with properties(array) 
-// Вернуть обьект со свойтвами указанными в массиве.
-const pickProps = (obj, arr) => {
-    let obj1 = {}
-    arr.forEach(element => {
-        if (arr === obj[element]) {
-            return obj1[element] = arr[i];
-        }
-        console.log(obj1);
-    });
-    return obj1;
-
-};
-const arr = ['name', 'age'];
-const obj = { name: 'Misha', age: 17, interest: 'football' }
-const result = pickProps(obj, arr);
-console.log(result);
-
-
-
-
-
-
 const mergeObjectsV1 = (obj1, obj2) => {
-    const result = Object.assign({}, obj1, obj2);
-    return result;
-};
-
+    let newObj = {};
+    Object.assign(newObj, obj1, obj2);
+    return newObj;
+}
 const mergeObjectsV2 = (obj1, obj2) => {
-    const result = Object.assign({}, obj2, obj1);
-    return result;
-};
-
+    let newObj = {};
+    Object.assign(newObj, obj2, obj1);
+    return newObj;
+}
 const mergeObjectsV3 = (obj1, obj2) => {
-    const result = {...obj1, ...obj2 };
-    return result;
-};
+    let newObj = {...obj1, ...obj2 };
+    return newObj;
+}
 
 const mergeObjectsV4 = (obj1, obj2) => {
-    const result = {...obj2, ...obj1 };
-    return result;
-};
+    let newObj = {...obj2, ...obj1 };
+    return newObj;
+}
 
 const user1 = {
     name: 'Bob',
@@ -52,7 +28,7 @@ const user2 = {
     student: false,
 };
 
-console.log(mergeObjectsV1(user1, user2));
-console.log(mergeObjectsV2(user1, user2));
-console.log(mergeObjectsV3(user1, user2));
-console.log(mergeObjectsV4(user1, user2));
+// console.log(mergeObjectsV1(user1, user2));
+// console.log(mergeObjectsV2(user1, user2));
+// console.log(mergeObjectsV3(user1, user2));
+// console.log(mergeObjectsV4(user1, user2));
