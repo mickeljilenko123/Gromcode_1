b673dc4b65f87aa0310f627dd56de12cfab6472d
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-03 22:48:52 +0300

    js

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-const mergeObjectsV1 = (obj1, obj2) => {
-    let newObj = {};
-    const objV1 = Object.assign(newObj, obj1, obj2);
-    return objV1;
-}
-const mergeObjectsV2 = (obj1, obj2) => {
-    let newObj = {};
-    const objV2 = Object.assign(newObj, obj2, obj1);
-    return objV2;
-}
-const mergeObjectsV3 = (obj1, obj2) => {
-
-    return {
-        ...obj1,
-        ...obj2
-    }
-}
-const mergeObjectsV4 = (obj1, obj2) => {
-    return {
-        ...obj2,
-        ...obj1
-    }
-}
+const getAdults = obj => {
+    let resA = {};
+    for (let key in obj) {
+        if (obj[key] >= 18) {
+            resA[key] = obj[key];
+        };
+    };
+    return resA;
+};
+const obj1 = { 'Ann': 19, 'Tom': 17, 'Bob': 20 }
+console.log(getAdults(obj1));
