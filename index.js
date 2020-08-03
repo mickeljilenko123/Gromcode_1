e8fb0e833e8c4eba1d17ae169f5b53e0da66155d
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-03 22:23:23 +0300

    js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const mergeObjectsV1 = (obj1, obj2) => {
     let newObj = {};
-    return Object.assign(newObj, obj1, obj2)
+    const objV1 = Object.assign(newObj, obj1, obj2);
+    return objV1;
 }
 const mergeObjectsV2 = (obj1, obj2) => {
     let newObj = {};
-    return Object.assign(newObj, obj2, obj1)
+    const objV2 = Object.assign(newObj, obj2, obj1);
+    return objV2;
 }
 const mergeObjectsV3 = (obj1, obj2) => {
+
     return {
         ...obj1,
         ...obj2
