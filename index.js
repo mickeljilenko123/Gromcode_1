e7bc6019517edce041404419abaa23770a865d4b
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-03 21:18:01 +0300

    js 8.6

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+//1. input: obj and array
+//2. output: obj with properties(array) 
+// Вернуть обьект со свойтвами указанными в массиве.
+const pickProps = (obj, arr) => {
+    let obj1 = {}
+    arr.forEach(element => {
+        if (arr === obj[element]) {
+            return obj1[element] = arr[i];
+        }
+        console.log(obj1);
+    });
+    return obj1;
+
+};
+const arr = ['name', 'age'];
+const obj = { name: 'Misha', age: 17, interest: 'football' }
+const result = pickProps(obj, arr);
+console.log(result);
+
+
+
+
+
+
 const mergeObjectsV1 = (obj1, obj2) => {
     const result = Object.assign({}, obj1, obj2);
     return result;
 };
 
-const mergeObjectsV2 = (obj1, obj2) => {
+const mergeObjectsV2 = (obj2, obj1) => {
     const result = Object.assign({}, obj2, obj1);
     return result;
 };
@@ -13,7 +37,7 @@ const mergeObjectsV3 = (obj1, obj2) => {
     return result;
 };
 
-const mergeObjectsV4 = (obj1, obj2) => {
+const mergeObjectsV4 = (obj2, obj1) => {
     const result = {...obj2, ...obj1 };
     return result;
 };
@@ -27,3 +51,8 @@ const user2 = {
     name: 'Patrick',
     student: false,
 };
+
+console.log(mergeObjectsV1(user1, user2));
+console.log(mergeObjectsV2(user1, user2));
+console.log(mergeObjectsV3(user1, user2));
+console.log(mergeObjectsV4(user1, user2));
