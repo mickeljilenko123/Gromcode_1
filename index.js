ff760e8238c058bdb1e2b8a06c38218acd5c362f
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-04 14:33:07 +0300

    js

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-const addPropertyV1 = (userData, userId) => {
-    userData.id = userId;
-    return userData
-}
-const addPropertyV2 = (userData, userId) => {
-    const id = { id: userId }
-    Object.assign(userData, id)
-    return userData
-}
-const addPropertyV3 = (userData, userId) => {
-    const id = { id: userId };
-    return Object.assign({}, userData, id)
-}
-const addPropertyV4 = (userData, userId) => {
-    return {
-        ...userData,
-        id: userId
-    }
+const buildObject = (keysList, valuesList) => {
+    let obj = {};
+    keysList.forEach((key, index) => obj[key] = valuesList[index])
+    return obj
 }
