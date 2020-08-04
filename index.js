70df4677ccd3b6564bb3673bdeb7f7add207bb54
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-04 11:41:41 +0300

    js

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-const getAdults = obj => {
-    let resA = {};
-    for (let key in obj) {
-        if (obj[key] >= 18) {
-            resA[key] = obj[key];
-        };
-    };
-    return resA;
-};
-const obj1 = { 'Ann': 19, 'Tom': 17, 'Bob': 20 }
-console.log(getAdults(obj1));
+function filterArray(arr, callback) {
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+
+        const callbackRes = callback(arr[i], i, arr);
+        if (callbackRes) {
+            result.push(arr[i])
+        }
+
+    }
+
+    return result;
+}
+
+
+//Testing
+
+
+let arr = [10, 30, 1, 6];
+
+const callback = (element) => {
+    const res = element > 20;
+    return res;
+}
+
+console.log(filterArray(arr, callback));
+
+
+// let arr = [10, 30, 1, 6];
+
+// const callback = (element, index, arr) => {
+
+//     console.log(element);
+//     console.log(index);
+//     console.log(arr);
+//     const res = element > 20;
+//     return res;
+// }
+
+// filterArray(array, callback);
