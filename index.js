89b700779bb8bf85cc2ba281cfcaf6112932f181
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-05 02:57:28 +0300

    js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const reverseArray = numbers =>
-    numbers.slice().sort((a, b) => b - a);
+    numbers.slice().reverse();
+
 const arr = [1, 4, 6, 12, 43, 7];
 if (!Array.isArray(arr)) null;
 console.log(reverseArray(arr));
