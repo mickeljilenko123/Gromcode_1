b036792e4925cfd7d12a16bea899ffab3d69fc8b
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-05 05:26:52 +0300

    js

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const users = {
     'Ann': 16,
     'Bob': 20,
 }
-const getAdults = usersObj => {
-    //Преобразовали обьект в массив масивов
-    const userArray = Object.entries(usersObj);
-    //Отфильтровать массив user[1] означает с со второго елемента.
-    const filteredUsersArray = userArray.filter(user => user[1] >= 18);
-    //через мап нам нужно откинуть значения и вернуть первый елемент
-    const userNames = filteredUsersArray.map(user => user[0]);
-    return userNames;
-}
-console.log(getAdults(users));
+
+const players = {
+    'John Doe': 19,
+    'Bob': 18,
+};
+
+const copyObj = obj => Object.assign({}, obj);
+console.log(copyObj(players));
+
+// const getAdults = usersObj => Object.entries(usersObj)
+// .filter(user => user[1] >= 18)
+// .map(user => user[0]);
+
+// console.log(getAdults(users));
