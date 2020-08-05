65568f0e0ec077fae6152961eaa53bd76cb7e48b
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-05 19:27:14 +0300

    js

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const rooms = {
-    room1: [
-        { name: 'room1 name1' }, { name: 'room1 name2' }, { name: 'room1 name3' }, { name: 'room1 name4' }
-    ],
-    room2: [
-        { name: 'room2 name1' }, { name: 'room2 name2' }, { name: 'room2 name3' }, { name: 'room2 name4' }
-    ],
-    room3: [
-        { name: 'room3 name1' }, { name: 'room3 name2' }, { name: 'room3 name3' }, { name: 'room3 name4' }
-    ],
+const customers = {
+    "customer-id-1": {
+        name: "William",
+        age: 54
+    },
+    "customer-id-2": {
+        name: "Tom",
+        age: 17
+    }
 };
 
-const getPeople = roomsObj => {
-    const roomArray = Object.values(roomsObj);
-    console.log(roomArray);
-    const filterArray = roomArray.flat();
-    // const flatArray = filterArray.flat(room => room);
-    const mapArray = filterArray.map(room => (room = room.name));
-    return mapArray;
+const getCustomersList = obj => Object.entries(obj)
+    .map(elem => ({ id: elem[0], ...obj[elem[0]] }))
+    .sort((a, b) => a.age - b.age);
 
-}
-console.log(getPeople(rooms));
+// const getCustomersList = obj => Object.entries(obj).forEach([key, value]);
+
+console.log(getCustomersList(customers));
+console.log(Object.values(customers));
