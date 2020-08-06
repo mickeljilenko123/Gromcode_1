d9291f485af124bdfbd86ed45a0edf6b6e438298
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-07 00:18:01 +0300

    JS

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-const getFiniteNumbers = arr => [...arr].filter(item => Number.isFinite(item));
-const getFiniteNumbersV2 = arr => [...arr].filter(el => isFinite(el));
-const getNaN = arr => [...arr].filter(el => Number.isNaN(el));
-const getNaNV2 = arr => [...arr].filter(el => isNaN(el));
-const getIntegers = arr => [...arr].filter(el => Number.isInteger(el));
-
-
-
-console.log(getFiniteNumbers(['15', 3, 'text', 9]));
-console.log(getFiniteNumbersV2(['15', 3, 'text', 9]));
-console.log(getNaN(['15', 3, 'text', 9]));
-console.log(getNaNV2(['15', 3, 'text', 9]));
-console.log(getIntegers(['15', 3, 'text', 9]));
+const getParsedIntegers = arr => arr.map(item => Number.parseInt(item));
+const getParsedIntegersV2 = arr => [...arr].map(item => parseInt(item));
+const getParsedFloats = arr => [...arr].map(item => Number.parseFloat(item));
+const getParsedFloatsV2 = arr => [...arr].map(item => parseFloat(item));
