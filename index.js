6fc81be3e2c674fd0182a0f6dad3359519be4293
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-07 00:32:33 +0300

    js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const multiRound = num => [
-    Math.floor(num * 100) / 100,
-    Math.round(num * 100) / 100,
-    Math.ceil(num * 100) / 100,
-    Math.trunc(num * 100) / 100, +num.toFixed(2)
-]
+const getMaxAbsoluteNumber = arr => {
+    if (!Array.isArray(arr) || arr.length === 0) return null;
+    const absoluteNumbersArray = arr.map(item => Math.abs(item));
+    return Math.max(...absoluteNumbersArray)
+}
