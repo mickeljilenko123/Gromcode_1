d6008e691b14275f79ee3295ebe2f75fedda578e
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-07 00:49:28 +0300

    js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const getMaxAbsoluteNumber = arr => {
-    if (!Array.isArray(arr) || arr.length === 0) return null;
-    const absoluteNumbersArray = arr.map(item => Math.abs(item));
-    return Math.max(...absoluteNumbersArray)
+const getTotalPrice = arr => {
+    let sum = 0;
+    arr.forEach(element => {
+        sum += element
+    });
+    return Math.floor(sum * 100) / 100;
 }
+
+const numbersArr = [17.159];
+
+console.log(getTotalPrice(numbersArr));
