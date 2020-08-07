c233b9b9d801bfffb533b5680c62433f595a9a64
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-07 15:19:00 +0300

    js

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-const getRandomNumbers = (len, start, end) => {
-    if (Math.floor(start) >= Math.floor(end))
-        return null;
-    let randomNum = Array(len).fill().map(len => Math.floor(Math.random() * (start - end + 1) + end))
-        // len.map(len => Math.floor(Math.random() * (start - end + 1) + end))
-    return randomNum;
+const superRound = num => {
+    return [Math.round(num), Math.floor(num), Math.ceil(num), Math.trunc(num), +num.toFixed(num)];
 }
-
-console.log(getRandomNumbers(10, 1, 7));
+console.log(superRound(17.2));
