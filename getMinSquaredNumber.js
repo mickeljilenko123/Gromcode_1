f2aaedeb57c8f5fc02bbee594afddbefd4fbf108
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-10 23:20:41 +0300

    js

--- getMinSquaredNumber.js
+++
@@ -1,7 +0,0 @@
-export const getMinSquaredNumber = arr => {
-    if (!Array.isArray(arr) || arr.length === 0)
-        return null
-    let absoluteValue = arr
-        .map(num => Math.abs(num * num))
-    return Math.min(...absoluteValue)
-};
