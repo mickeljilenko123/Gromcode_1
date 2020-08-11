5f21836f7089992852bc288a4a3b7032e344aee8
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-11 16:00:35 +0300

    js

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
- let num = 4;
+let memory = 0;
 
- function add(n) {
-     console.log(`${n} + ${num}`);
- }
- add(4);
+function add(num) {
+    memory += num;
+};
+
+function decrease(num) {
+    memory -= num;
+};
+
+function reset(num) {
+    memory = num;
+};
+
+function getMemo(num) {
+    return memory;
+};
+
+// console.log(add(4));
+// console.log(decrease(4));
+// console.log(reset(4));
+// console.log(getMemo(4));
