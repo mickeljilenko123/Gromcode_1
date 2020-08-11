ed187c868e062ba6b7bffea5362a8c9d2e37927b
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-11 12:07:03 +0300

    js

---
+++ index.js
@@ -0,0 +1,17 @@
+let message = 'Just learn it';
+
+function sendMessage(name) {
+
+    const sender = 'Gromcode';
+    console.log(`${name}, ${message}! Your ${sender}`);
+};
+
+function setMessage(text) {
+    message = text;
+};
+
+// sendMessage('Ann');
+
+// setMessage('Good job');
+
+// sendMessage('Ann');
