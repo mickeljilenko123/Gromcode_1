5fb3f95bcab0d40e720868a73ec6f57eca8b49b8
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-11 12:09:42 +0300

    js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let message = 'Just learn it';
 
-function sendMessage(name) {
+export function sendMessage(name) {
 
     const sender = 'Gromcode';
     console.log(`${name}, ${message}! Your ${sender}`);
 };
 
-function setMessage(text) {
+export function setMessage(text) {
     message = text;
 };
 
