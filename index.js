ba7eddb333c1b302f55899a3f118a38e923bb1dd
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-12 17:49:07 +0300

    js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var foo = 1;
 
-export default function bar() {
+function bar() {
     if (!foo) {
         let foo = 10;
     }
@@ -9,3 +9,5 @@ export default function bar() {
 }
 
 var foo = bar();
+
+export default foo;
