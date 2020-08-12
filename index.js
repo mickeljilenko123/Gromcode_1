1527639e1c22ec4424d9bd97ab7181bd7f65aa67
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-12 14:59:44 +0300

    js

--- index.js
+++ index.js
@@ -1,51 +1,12 @@
-export function createLogger() {
-    let memory = [];
-
-    function warn(elem) {
-        memory.push({
-            message: elem,
-            dateTime: new Date(),
-            type: 'warn',
-        });
-    };
-
-    function error(elem) {
-        memory.push({
-            message: elem,
-            dateTime: new Date(),
-            type: 'error',
-        });
-    };
-
-    function log(elem) {
-        memory.push({
-            message: elem,
-            dateTime: new Date(),
-            type: 'log',
-        });
-    };
-
-    function getRecords(type) {
-        // console.log(type)
-        // if (type !== undefined) {
-        //     let result = memory.filter(el => el.type === type);
-        //     return result;
-        // }
-
-        if (type !== undefined) {
-            let result = memory.filter(el => el.type === type)
-                .sort((a, b) => b.dateTime - a.dateTime);
-            console.log(result)
-            return result;
-        } else {
-            return memory.sort((a, b) => b.dateTime - a.dateTime);
-        };
-    };
-
-    return {
-        warn,
-        error,
-        log,
-        getRecords,
-    };
-};
+ export function createArrayOfFunctions(n) {
+     let arr = [];
+     if (typeof(n) !== 'numbers') return null;
+     if (n === undefined) return arr;
+     for (let i = 0; i < n; i++) {
+         arr[i] = function() {
+             return i;
+         }
+     }
+     return arr;
+ }
+ console.log(createArrayOfFunctions(5));
