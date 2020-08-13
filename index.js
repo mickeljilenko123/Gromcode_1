9718be96911da671e0b6378aee1f5b2811e137cd
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-13 18:00:13 +0300

    js

--- index.js
+++ index.js
@@ -22,18 +22,81 @@
 // };
 // callbackPrompt.showDeferredPrompt(1000);
 
-export function defer(func, ms) {
-    return function() {
-        setTimeout(() => func.call(this, ...arguments), ms);
-    }
-}
-
-const user = {
+/* ===> 1 <=== */
+const student = {
     name: 'Tom',
-    sayHi() {
-        console.log(`Hi, I'am ${this.name}!`)
+};
+
+export function sayName() {
+    console.log(this.name);
+}
+
+
+// вызовите ф-цию sayName так, чтобы в консоль вывелось имя студента 
+// ... your code here
+sayName.call(student);
+
+// вызовите ф-цию sayName так, чтобы в консоль вывелось имя 'Bruce' (используйте другой объект)
+// ... your code here
+
+sayName.call({ name: 'Bruce' });
+
+/* ===> 2 <=== */
+const company = {
+    companyName: 'Microsoft'
+};
+
+function greeting(firstName, lastName) {
+    console.log(`Hello, ${firstName} ${lastName}. Welcome to the ${this.companyName}`);
+}
+
+// вызовите ф-цию greeting так, чтобы в консоль вывелось 
+// 'Hello, Bob Marley. Welcome to the Microsoft'
+
+// используйте объект company
+greeting.apply(company, ['Bob', 'Marley']);
+
+
+/* ===> 3 <=== */
+const country = {
+    countryName: 'Ukraine',
+    capital: 'Kyiv'
+};
+
+function getPopulation(population) {
+    console.log(`Population in ${this.countryName} is ${population}`);
+}
+getPopulation.apply(country, [43000]);
+
+// вызовите ф-цию getPopulation так, чтобы она вернула 
+// 'Population in Ukraine is 43000'
+// 43000 передавайте в виде числа
+// используйте объект country
+// результат работы ф-ции getPopulation присвойте в переменную и выведите в консоль
+// ... your code here
+
+
+
+/* ===> 4 <=== */
+const transaction = {
+    amount: 1200,
+    operation: 'sell',
+    currency: 'USD',
+    exchange: 'NYSE',
+    printTransaction() {
+        console.log(`${this.amount} ${this.currency} - ${this.operation} on ${this.exchange}`);
     }
 }
-const deferredHi = defer(user.sayHi, 1000);
 
-deferredHi.call({ name: "bob" });
+const anotherTransaction = {
+    amount: 400,
+    operation: 'buy',
+    currency: 'USD',
+    exchange: 'NASDAQ',
+};
+
+// вызовите метод transaction.printTransaction так, чтобы в консоль вывелось
+// '400 USD - buy on NASDAQ'
+// используйте объект anotherTransaction как контекст
+const func = transaction.printTransaction;
+func.call(anotherTransaction);
