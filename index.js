e65f42f49ef9e1d7a23589bc372be71ad30cfc37
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-13 14:39:07 +0300

    js

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-export const user = {
-    firstName: 'John',
-    lastName: 'Doe',
-    getFullName() {
-        return (`${this.firstName} ${this.lastName}`);
-    }
-}
+// const user = {
+//     name: 'John',
+//     sayHi(age, message) {
+//         console.log(`${message}. I'm ${this.name}. I'm ${age} years old`);
+//     }
+// }
 
-user.getFullName();
+// const func = user.sayHi;
+
+// func.apply(user, [17, 'Hello']);
+
+
+const callbackPrompt = {
+    message: 'Tell me your number',
+    showPrompt() {
+        const phoneNumber = prompt(this.message);
+        console.log(phoneNumber);
+    },
+    showDeferredPrompt(ms) {
+        setTimeout(this.showPrompt.bind(this), ms);
+    }
+};
+
+callbackPrompt.showDeferredPrompt(1000);
+
+export { callbackPrompt };
