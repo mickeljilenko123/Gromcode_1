106a2c066be30a028d13693d64af308fd01f8efc
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-13 12:56:53 +0300

    js

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
-//hoisting example 1
-console.log(message);
-var message = 'Hoisting is here!';
-
-
-//hoisting example 2
-var a = 55;
-console.log(a);
-
-if (a) {
-    let a = 1;
-
-    console.log(a);
+const user = {
+    firtsName: 'John',
+    lastName: 'Doe',
+    getFullName() {
+        console.log(`Hi, I am ${this.firtsName} ${this.lastName}`);
+    }
 }
 
-console.log(a);
-
-
-//hoisting example 3
-var a = 77;
-console.log(a);
-
-function print() {
-    let a = 2;
-    console.log(a);
-}
-print();
-
-console.log(a);
+user.getFullName();
