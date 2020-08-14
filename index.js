ee3df56d9f6a5b58733533d616a6eb80b1ed8157
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-14 10:05:04 +0300

    js

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-export const user = {
-    firstName: 'John',
-    lastName: 'Doe',
+const user = {
+    firstName: "Roland",
+    lastName: "Diskein",
     getFullName() {
-        // console.log(`${this.firstName} ${this.lastName}`);
         return `${this.firstName} ${this.lastName}`;
-    }
-}
+        // console.log(`${this.firstName} ${this.lastName}`);
+    },
+    setFullName(getFullName) {
+        // console.log(user.firstName);
+        let a = getFullName.split(" ");
+        console.log(a[0]);
+        // let a = this.getFullName().split(" ");
+        // console.log(a[0]);
 
-const fullName = user.getFullName;
-fullName.call(user);
+        this.firstName = a[0];
+        this.lastName = a[1];
+
+        // console.log((this.firstName = a[0]));
+        // console.log((this.lastName = a[1]));
+    },
+};
+
+// user.getFullName();
+
+// user.setFullName();
+
+export { user };
