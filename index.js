fec4c4b5dbb2dfcabb50550dbd1b4ba919a289ec
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-14 13:20:19 +0300

    js

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 
 const event = {
     guests: [
-        { name: 'Tom', email: 't@gmail.com', age: 17 },
-        { name: 'Tom', email: 't@gmail.com', age: 18 }
+        { name: 'Tom', email: 'exampe@server.com', age: 17 },
+        { name: 'John', email: 'exampe@server.com', age: 18 }
     ],
     message: 'Welcome to the party!',
     getInvintations() {
         return this.guests
             .filter(({ age }) => age >= 18)
             .map(({ name, email }) => ({
+                email,
                 text: `Dear ${name}! ${this.message}`,
-                email
             }));
     }
 }
