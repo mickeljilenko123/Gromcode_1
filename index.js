2f057711f0aba294dd6886a10d006f179bf5c2b1
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-17 11:02:33 +0300

    js

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-const user = {
-    firstName: 'Bruce',
-    lastName: 'Wayne',
-    get fullName() {
-        return `${this.firstName} ${this.lastName}`;
-        // console.log(`${this.firstName} ${this.lastName}`);
-    },
-    set fullName(value) {
-        const [firstName, lastName] = value.split(' ');
-        this.firstName = firstName;
-        this.lastName = lastName;
-    },
+function User(name, age) {
+    this.name = name;
+    this.age = age;
 };
-// user.fullName
 
-// user.fullName = 'Tom Lee';
+User.prototype.sayHi = function() {
+    console.log(`Hi, I am ${this.name}`);
+};
 
-// user.fullName
+User.prototype.requestNewPhoto = function() {
+    console.log(`New photo request was sent for ${this.name}`);
+};
 
-export default user;
+User.prototype.setAge = function(age) {
+    this.age = age;
+    // console.log(age);
+
+    if (age < 0) {
+        return false;
+    }
+
+    if (age >= 25) {
+        console.log(`New photo request was sent for ${this.name}`);
+        // this.age = age;
+    }
+
+    return age;
+};
+
+const user1 = new User("Olaf", 3);
+console.log(user1);
+
+// user1.sayHi()
+// user1.setAge()
+// user1.requestNewPhoto()
+
+export {
+    User
+};
