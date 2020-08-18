ba439dee3ab7e3364e40e0dbac3b98828a38726e
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-18 15:56:26 +0300

    js

--- index.js
+++ index.js
@@ -1,48 +1,15 @@
-const tasks = [{
-        text: 'Buy milk',
-        done: false
-    },
-    {
-        text: 'Pick up Tom from airport',
-        done: false
-    },
-    {
-        text: 'Visit party',
-        done: false
-    },
-    {
-        text: 'Visit doctor',
-        done: true
-    },
-    {
-        text: 'Buy meat',
-        done: true
-    },
-];
-
-const renderListItem = listItems => {
-    const listElement = document.querySelector('.list');
-
-    const listItemsElements = listItems
-        .sort((a, b) => a.done - b.done)
-        .map(({
-            text,
-            done
-        }) => {
-            const listItemElement = document.createElement('li');
-            listItemElement.classList.add('list__item');
-            if (done) {
-                listItemElement.classList.add('list__item_done');
-            }
-            const checkboxElement = document.createElement('input');
-            checkboxElement.setAttribute('type', 'checkbox');
-            checkboxElement.checked = done;
-            checkboxElement.classList.add('list__item-checkbox');
-            listItemElement.append(checkboxElement, text);
-
-            return listItemElement;
-        })
-    listElement.append(...listItemsElements);
+export const getTitleElement = () => {
+    const titleElem = document.querySelector('.title');
+    console.dir(titleElem);
+    return titleElem;
 };
 
-console.log(renderListItem(tasks));
+getTitleElement();
+
+export const getInputElement = () => {
+    const inputElem = document.querySelector('input[type="text"]');
+    console.dir(inputElem);
+    return inputElem;
+};
+
+getInputElement();
