507105bf95977fa9d257aa4e283804d978746cfe
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-19 03:41:04 +0300

    js

--- index.js
+++ index.js
@@ -4,3 +4,24 @@ export const getTitle = () => {
     return elementsList;
 }
 getTitle();
+
+export const getDescription = () => {
+    const elementsList = document.querySelector('.about').innerText;
+    console.dir(elementsList);
+    return elementsList;
+}
+getDescription();
+
+export const getPlans = () => {
+    const elementsList = document.querySelector('.plans').innerHTML;
+    console.dir(elementsList);
+    return elementsList;
+}
+getPlans();
+
+export const getGoal = () => {
+    const elementsList = document.querySelector('.goal').outerHTML;
+    console.dir(elementsList);
+    return elementsList;
+}
+getGoal();
