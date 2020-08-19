60935ddb0e79aec63dadce297bb5d58731f5c473
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-19 14:50:14 +0300

    js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
- export const createButton = (buttonText) => {
-     const b = document.querySelector('body');
-     const buttonCreate = document.createElement('button');
-     b.append(buttonCreate);
-     buttonCreate.textContent = buttonText;
-     return buttonCreate;
- };
+export const finishList = () => {
+    const addElem = document.createElement('ul');
+    addElem.classList.add('.list');
 
- createButton('Submit');
+    const addList = document.createElement('li');
+    const addList2 = document.createElement('li');
+    const addList3 = document.createElement('li');
+    addList3.classList.add('special');
+    const addList4 = document.createElement('li');
+    addElem.append(addList, addList2, addList3, addList4);
+    addList.textContent = '3';
+    addList2.textContent = '2';
+    addList3.textContent = '5';
+    addList4.textContent = '7';
+    console.log(addElem);
+
+}
+finishList();
