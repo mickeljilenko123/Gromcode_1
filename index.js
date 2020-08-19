e54d2beeced37fca9790f27c52d8be850484033e
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-19 13:00:09 +0300

    js

--- index.js
+++ index.js
@@ -1,38 +1,8 @@
-// const timer = {
-//     secondPassed: 0,
-//     minsPassed: 0,
-//     intervalId: null,
-
-//     startTimer() {
-//         this.intervalId = setInterval(() => {
-//             console.log(this);
-//             this.secondPassed += 1;
-
-//             if (this.secondPassed === 60) {
-//                 this.minsPassed += 1;
-//                 this.secondPassed = 0;
-//             }
-
-//         }, 1000);
-
-//     },
-//     stopTimer() {
-//         clearInterval(this.intervalId);
-//     },
-//     getTime() {
-
-//     },
-//     reset() {
-
-//     }
-// }
-
-// timer.startTimer();
-// // timer.stopTimer();
-
-export function setButton() {
+const setButton = (buttonText) => {
     const setBut = document.querySelector('body');
-    setBut.innerHTML = '<button>buttonText</button>';
+    setBut.innerHTML = `<button>${buttonText}</button>`;
     console.log(setBut);
+    return setBut;
 }
-setButton();
+const buttonText = 'Как дела?';
+setButton(buttonText);
