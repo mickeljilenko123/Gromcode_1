10341621c61e3ee5249a7424fffe1f23a46c9e83
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-19 03:50:00 +0300

    js

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-export const getTitle = () => {
+const getTitle = () => {
     const elementsList = document.querySelector('.title').textContent;
     console.dir(elementsList);
     return elementsList;
 }
 getTitle();
 
-export const getDescription = () => {
-    const elementsList = document.querySelector('.about').innerText;
-    console.dir(elementsList);
-    return elementsList;
-}
-getDescription();
+// const getDescription = () => {
+//     const elementsList = document.querySelector('.about').innerText;
+//     console.dir(elementsList);
+//     return elementsList;
+// }
+// getDescription();
 
-export const getPlans = () => {
-    const elementsList = document.querySelector('.plans').innerHTML;
-    console.dir(elementsList);
-    return elementsList;
-}
-getPlans();
+// const getPlans = () => {
+//     const elementsList = document.querySelector('.plans').innerHTML;
+//     console.dir(elementsList);
+//     return elementsList;
+// }
+// getPlans();
 
-export const getGoal = () => {
-    const elementsList = document.querySelector('.goal').outerHTML;
-    console.dir(elementsList);
-    return elementsList;
-}
-getGoal();
+// const getGoal = () => {
+//     const elementsList = document.querySelector('.goal').outerHTML;
+//     console.dir(elementsList);
+//     return elementsList;
+// }
+// getGoal();
