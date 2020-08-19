2d89c9573812ab1e87dba41ffc07abd7f20c5380
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-19 03:56:41 +0300

    js

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-function getTitle() {
+export function getTitle() {
     return document.querySelector('.title').textContent;
 }
-getTitle();
 
-function getDescription() {
+
+export function getDescription() {
     return document.querySelector('.about').innerText;
 }
-getDescription();
 
-function getPlans() {
+
+export function getPlans() {
     return document.querySelector('.plans').innerHTML;
 }
-getPlans();
 
-function getGoal() {
+
+export function getGoal() {
     return document.querySelector('.goal').outerHTML;
 }
-getGoal();
