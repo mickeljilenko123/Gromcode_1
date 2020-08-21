b04740cf0158e592c451acd36a55fd2be5fbbc96
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-21 14:25:54 +0300

    js

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-const checkBox = document.querySelector('.task-status');
+const inputValue = document.querySelector('.search__input');
 
-checkBox.addEventListener('change', () => {
-    checkBox.checked ? console.log(true) : console.log(false)
-});
-
-// const a = () => {
-//     if (checkBox.checked) {
-//         console.log(true);
-//     }
-//     else {
-//         console.log(false);
-//     }
-// } 
-
-// checkBox.addEventListener('change', a);
+document.querySelector('.search__btn')
+    .addEventListener('click', () => {
+        console.log(inputValue.value)
+    })
