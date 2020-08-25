bc81a5bdc21f97ebeead10008a99572a8115190d
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-25 13:44:51 +0300

    js

--- index.js
+++ index.js
@@ -1,61 +1,12 @@
-// 'use strict'
-function printMessage(age, city) {
-    console.log(this);
-    console.log(`Hello ${this.name}, email ${this.email}, age: ${age}, city: ${city}`);
-}
+const checkBox = document.querySelector('.task-status');
 
-let user = {
-    name: "Alex",
-    email: "gmail.com",
-}
-
-// bind algs
-//input: obj, aarg1,..., argN
-//output: function
-
-// const pmBinded = printMessage.bind(user);
-// pmBinded(100, 'Lviv')
-
-
-// bind algs
-//input: func, obj, aarg1,..., argN
-//output: function
-// const myBind = (func, context, ...args) => {
-//   console.log('Step1')
-//   return function() {
-//     console.log('Step1')
-//     return func.call(context, ...args)
-//   }
-// }
-// const myBind = myBind(printMessage, user, 200, 'Kiyv');
-
-// const myBind1 = (func, context, ...args) => {
-//   console.log('Step1')
-//   return function(...targetArgs) {
-//     console.log('Step1')
-//     return func.call(context, ...args, ...targetArgs)
-//   }
-// }
-
-// const printMyBind1 = myBind1(printMessage, user);
-// printMyBind1(200, 'Kiyv');
-// const printMyBind2 = myBind1(printMessage, user, 200);
-// printMyBind2( 'Kiyv')
-
-const myBindSuperBind = (func, context, ...args) => {
-    console.log("step1")
-    return function() {
-        console.log("step2");
-
-        const copyContext = {...context }
-        copyContext.tempFunction = func;
-        copyContext.tempFunction(...args);
+const foo = (event) => {
+    const getEvent = event.target;
+    if (getEvent.checked) {
+        console.log(true);
+    } else {
+        console.log(false);
     }
+};
 
-}
-
-const printMyBind4 = myBindSuperBind(printMessage, user, 200, 'Dnipro');
-// console.log(printMyBind4)
-printMyBind4();
-
-console.log(user)
+checkBox.addEventListener('change', foo);
