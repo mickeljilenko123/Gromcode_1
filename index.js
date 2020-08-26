f080c5ddb104220c78eb91e1cd37a5141c4bc7c5
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-26 20:59:59 +0300

    js

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const formatter = new Intl.DateTimeFormat('en', {
-    timeZone: 'UTC',
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: false,
-});
 
-const getGreenwichTime = date => formatter.format(date);
+const startNewDate = new Date();
+const endNewDate = new Date(2021, 1, 10, 10, 10, 10);
 
-console.log(getGreenwichTime(new Date()));
+const getDiff = (startDate, endDate) => {
 
-export { getGreenwichTime };
+    console.log(startDate)
+    let getNewDate = new Date(startDate - endDate);
+
+    if (startDate < endDate)
+    getNewDate = new Date(endDate) - new Date(startDate);
+
+    const newDay = parseInt(getNewDate / (1000 * 60 * 60 * 24));
+    const newHour = parseInt((getNewDate / (1000 * 60 * 60)) % 24);
+    const newMin = parseInt((getNewDate / (1000 * 60)) % 60);
+    const newSec = parseInt((getNewDate / 1000) % 60);
+
+    // let newDay = getNewDate.getDate();
+    // let newHour = getNewDate.getHours();
+    // let newMin = getNewDate.getMinutes();
+    // let newSec = getNewDate.getSeconds();
+
+    // console.log(`${newDay}d ${newHour}h ${newMin}m ${newSec}s`);
+    return `${newDay}d ${newHour}h ${newMin}m ${newSec}s`;
+};
+
+const myDate = getDiff(startNewDate, endNewDate);
+
+console.log(myDate);
+
+export { getDiff };
+
