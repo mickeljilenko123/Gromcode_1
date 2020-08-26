981d6636b59bf654a306b157987946b78f9514c9
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-26 12:55:52 +0300

    js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const generateNumbersRange = (from, to) => {
     const result = [];
 
@@ -7,64 +9,65 @@ const generateNumbersRange = (from, to) => {
     return result;
 };
 
-const getLineSeats = () => generateNumbersRange(1, 10)
-    .map(
-        seatNumber =>
-            `<div class="sector__seat"
-             data-seat-number="${seatNumber}">
-             </div>`
-    )
-    .join("");
-
-const getSectorLines = () => {
-    const seatsString = getLineSeats();
-
+const getLineSeats = () => {
     return generateNumbersRange(1, 10)
         .map(
-            lineNumber =>
-                `<div class="sector__line"
-             data-line-number="${lineNumber}">
-             ${seatsString}
-             </div>`
+            (seatNumber) => `
+    <div 
+      class="sector__seat" 
+      data-seat-number="${seatNumber}"
+    ></div>
+  `
         )
         .join("");
 };
 
-const arenaElem = document.querySelector('.arena');
+const getSectorLines = () => {
+    const seatsString = getLineSeats();
+    return generateNumbersRange(1, 10)
+        .map(
+            (lineNumber) => `
+    <div 
+      class="sector__line" 
+      data-line-number="${lineNumber}"
+    >${seatsString}</div>
+  `
+        )
+        .join("");
+};
+
+const arenaElem = document.querySelector(".arena");
 
 const renderArena = () => {
-    // const arenaElem = document.querySelector(".arena");
     const linesString = getSectorLines();
 
     const sectorsString = generateNumbersRange(1, 3)
         .map(
-            sectorNumber =>
-                `<div class="sector"
-             data-sector-number="${sectorNumber}">
-             ${linesString}
-             </div>`
+            (sectorNumber) => `
+    <div 
+      class="sector" 
+      data-sector-number="${sectorNumber}"
+    >${linesString}</div>
+    `
         )
         .join("");
 
     arenaElem.innerHTML = sectorsString;
 };
 
-const onSeatSelect = event => {
-    const isSeat = event.target.classList.contains('sector__seat');
-
-    if (!isSeat) {
-        return;
-    };
+const onSeatSelect = (event) => {
+    const isSeat = event.target.classList.contains("sector__seat");
+    if (!isSeat) return;
 
     const seatNumber = event.target.dataset.seatNumber;
-    const lineNumber = event.target.closest('.sector__line').dataset.lineNumber;
-    const sectorNumber = event.target.closest('.sector').dataset.sectorNumber;
+    const lineNumber = event.target.closest(".sector__line").dataset.lineNumber;
+    const sectorNumber = event.target.closest(".sector").dataset.sectorNumber;
 
-    const selectedSeatElem = document.querySelector('.board__selected-seat');
+    const selectedSeatElem = document.querySelector(".board__selected-seat");
 
     selectedSeatElem.textContent = `S ${sectorNumber} - L ${lineNumber} - S ${seatNumber}`;
 };
 
-arenaElem.addEventListener('click', onSeatSelect);
+arenaElem.addEventListener("click", onSeatSelect);
 
 renderArena();
