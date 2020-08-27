3540ed3283dd0a7e7750803f532dee0377a21fbb
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-27 11:47:47 +0300

    js

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
-const students = [
-    { name: 'Sem', birthDate: '01/10/2000', },
-    { name: 'Alex', birthDate: '05/12/2001', },
-    { name: 'Robert', birthDate: '10/05/2003', },
-    { name: 'Monica', birthDate: '01/05/2002', },
-];
+//input: string css selector
+//output: HtmlElement
 
-// const studentsBirthDays = (students) => {
-//     const allMonth = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-//     // ===========================================================
-//     students.forEach(elem => {})
+const btns = document.querySelectorAll('.btn');
 
+btns.forEach(btn => {
+    // const obj = {
+    //     name: 'Some user',
+    // }
+    // btn.addEventListener('click', hendleClick.bind(obj));
+    btn.addEventListener('click', hendleClick);
+});
 
-const studentsBirthDays = allStudents => {
-
-    const allMonth = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-    const sortAllStudents = allStudents.sort((a, b) => new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate());
-    // console.log(new Date(allStudents.birthDate).getDate())
-    console.log(sortAllStudents)
-
-    const getDatesOfStudents = sortAllStudents.reduce((acc, { name, birthDate }) => {
-        const monthsName = allMonth[new Date(birthDate).getMonth()];
-        return { ...acc, [monthsName]: acc[monthsName] ? acc[monthsName].concat(name) : [name] };
-    }, {});
-
-    console.log(getDatesOfStudents)
-    return getDatesOfStudents;
+function hendleClick(event) {
+    // console.log(this);
+    // console.log(`Hello, ${this.name}`);
+    console.log(event.target.innerText);
 };
-
-studentsBirthDays(students)
-
-export { studentsBirthDays };
