db134e60ef8e3bc1cc30483fd5864e0b5b0523b0
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-28 15:49:09 +0300

    js

---
+++ index.js
@@ -0,0 +1,29 @@
+//calc(3).add(2).mult(4).div(10).substract(5).result();
+const calc = initValue => {
+    let result = initValue;
+    const calculator = {
+        add(value) {
+            result += value;
+            return this;
+        },
+        substract(value) {
+            result -= value;
+            return this;
+        },
+        mult(value) {
+            result *= value;
+            return this;
+        },
+        div(value) {
+            result /= value;
+            return this;
+        },
+        result() {
+            return result;
+        }
+    };
+    return calculator;
+};
+
+const result = calc(3).add(10).substract(4).mult(2).div(6).result();
+console.log(result);
