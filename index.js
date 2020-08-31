a9f93608c0e5ed94dca9bbfd7d5f4d3af68a83a9
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-31 15:21:39 +0300

    js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const mult = a => b => a * b;
+export const mult = a => b => a * b;
 const add3 = mult(3);
 const res = add3(5);
 console.log(res);
 
-const twice = a => a + a;
+export const twice = a => a + a;
 const add4 = twice(4);
 console.log(add4);
 
-const triple = a => a + a + a;
+export const triple = a => a + a + a;
 const add5 = triple(2);
 console.log(add5);
