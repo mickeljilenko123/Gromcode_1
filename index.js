ac0e7f2aaebde444305837a90af4b69eaf63a53c
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-31 19:45:29 +0300

    js

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-import { renderList } from './render.js';
-import { tasks } from './storage.js';
-import { createTaskBoard, checkthisDone } from './gateway.js';
+const getMethodsNames = {
+    years: 'getFullYear',
+    months: 'getMonth',
+    days: 'getDate',
+    hours: 'getHours',
+    minutes: 'getMinutes',
+    seconds: 'getSeconds',
+    milliseconds: 'getMilliseconds',
+};
 
-renderList(tasks);
+const setMethodsNames = {
+    years: 'setFullYear',
+    months: 'setMonth',
+    days: 'setDate',
+    hours: 'setHours',
+    minutes: 'setMinutes',
+    seconds: 'setSeconds',
+    milliseconds: 'setMilliseconds',
+};
 
-const listElem = document.querySelector('.list');
-listElem.addEventListener('click', checkthisDone);
+const shmoment = date => {
+    let result = new Date(date);
 
-const createBtn = document.querySelector('.create-task-btn');
-createBtn.addEventListener('click', createTaskBoard);
+    const calculator = {
+        add(units, value) {
+            const currentUnitValue = result[getMethodsNames[units]]();
+            result = new Date(
+                result[setMethodsNames[units]](currentUnitValue + value),
+            );
+            return this;
+        },
+        subtract(units, value) {
+            return this.add(units, -value);
+        },
+        result() {
+            return result;
+        },
+    };
+
+    return calculator;
+};
+
+const result = shmoment(new Date(2020, 1, 2, 17, 10)).add('minutes', 8).add('days', 3).subtract('years', 1).result();
+
+console.log(result)
+
+export { shmoment };
