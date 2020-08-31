bf3cae01e3715441a9fd81f758a653f778561315
mickeljilenko123 <mickeljilenko@gmail.com>
2020-08-31 16:14:51 +0300

    js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-export const mult = a => b => a * b;
-const add3 = mult(3);
-const res = add3(5);
-console.log(res);
+const add3 = value => value + 3;
+const mult2 = value => value * 2;
+const div4 = value => value / 4;
 
-export const twice = a => a + a;
-const add4 = twice(4);
-console.log(add4);
+export const compose = (...funcs) => value => {
+    return funcs.reduce((acc, func) => func(acc), value);
+};
 
-export const triple = a => a + a + a;
-const add5 = triple(2);
-console.log(add5);
+const doEverything = compose(
+    add3,
+    mult2,
+    div4,
+);
+console.log(doEverything(3));
