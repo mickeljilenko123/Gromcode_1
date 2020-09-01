1cb513c713a3e7bbfa4e7fbe1f2a1f2b1f2b9fa2
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-01 17:13:06 +0300

    js

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-
-const addImage = imgSrc => {
-    const p = new Promise((resolveCb, rejectCb) => {
-        const imgElem = document.createElement('img');
-        imgElem.setAttribute('alt', 'My Photo');
+export const addImage = (imgSrc) => {
+    const p = new Promise((resolve, reject) => {
+        const imgElem = document.createElement("img");
         imgElem.src = imgSrc;
-
-        const containerElem = document.querySelector('.page');
-        containerElem.append(imgElem);
-
+        imgElem.setAttribute("alt", "My Photo");
+        const containerElement = document.querySelector(".page");
+        containerElement.append(imgElem);
         const onImageLoaded = () => {
             const { width, height } = imgElem;
-            resolveCb({ width, height });
+            resolve({ width, height });
         };
+        imgElem.addEventListener("load", onImageLoaded);
 
-        imgElem.addEventListener('load', onImageLoaded);
-
-        imgElem.addEventListener('error', () => rejectCb(new Error('Image load failed')))
+        imgElem.addEventListener("error", () =>
+            reject(new Error("Image load is failed..."))
+        );
     });
     return p;
 };
 
-const imgSrc = 'https://server.com/image.png';
+const imgSrc =
+    "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg";
 
 const resultPromise = addImage(imgSrc);
-
-resultPromise.catch(error => console.log(error));
-
-
-export { addImage };
+resultPromise.then((data) => onImageLoaded(data));
+const onImageLoaded = (data) => {
+    const { width, height } = data;
+    const sizeElem = document.querySelector(".image-size");
+    sizeElem.textContent = `${width} x ${height}`;
+};
+resultPromise.catch((error) => console.log(error));
