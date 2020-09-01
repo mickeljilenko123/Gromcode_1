8528f64249ec1d372b8ec4c9d81dcbe5d9b89617
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-01 21:52:46 +0300

    js

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-/* здесь мы создаем промис, который резолвит объект { name: 'Tom', age: 17 } через 1 секунду */
-const userDataPromise = new Promise((resolve) => {
-    setTimeout(() => {
-        resolve({ name: "Tom", age: 17 });
-    }, 1000);
-});
-// console.log(userDataPromise);
+/* ===> 1 <=== */
+
 /*
- * Выведите в консоль переменную userDataPromise
- * Ответьте себе на вопрос, какой тип данных имеет переменная userDataPromise?
+ * successPromise должен зарезолвить число 67
+ * Ответьте себе на вопрос, какой тип данных имеет переменная successPromise
  */
 
-/* ...code here */
-// userDataPromise.then((data) => console.log(data));
-console.log(userDataPromise);
-/*
- * Выведите в консоль переменную userData в обработчике промиса
- * Ответьте себе на вопрос, что находится в переменной userData?
- */
-userDataPromise.then(function onSuccess(userData) {
+const successPromise = new Promise((resolve) => {
     /* ...code here */
-    console.log(userData);
+    // const a = 67;
+    // resolve(a);
+    resolve(67);
+});
+console.log(typeof successPromise);
+/*
+ * допишите обработчик успешного промиса (аргументы и тело ф-ции onSuccess)
+ * чтобы в консоль вывелся квадрат числа, которое резолвиться из successPromise
+ */
+
+successPromise.then(function onSuccess(number) {
+    /* ...code here */
+    console.log(number * number);
 });
 
 /*
- * подпишитесь на успешное выполнение промиса userDataPromise
- * используйте метод .then
- * в обработчике промиса (ф-ция внутри .then() ) выведите в консоль строку 'My name is Tom. I am 17 years old'
- * Tom и 17 достаньте с данных, которые приходят в ф-цию onSuccess
+ * в обработчике ошибок промиса (ф-ция onError внутри .catch()) выведите в консоль текст 'I am an error'
+ * текст вывелся в консоль?
+ * подумайте почему
  */
-
-userDataPromise.then(function onSuccess(userData) {
+successPromise.catch(function onError() {
     /* ...code here */
-    console.log(`My name is ${userData.name}. I am ${userData.age} years old`);
+    console.log(new Error("I am an error"));
 });
