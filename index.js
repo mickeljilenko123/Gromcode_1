97c9628f69f13ce75a526c7b4f4356abfba1dc17
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-15 16:11:07 +0300

    js

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
-const baseUrl = 'https://5f60907d90cf8d0016557f3a.mockapi.io/api/v1/users';
+const baseUrl = 'https://5e9e6e6ffb467500166c3f84.mockapi.io/api/v1/users';
 
 /* getUsersList code here */
-const getUsersList = () =>
-    fetch(baseUrl).then((response) => response.json());
+const getUsersList = () => {
+    return fetch(baseUrl)
+        .then(response => response.json())
+        .then(result => console.log(result))
+};
 
+// getUsersList();
 
 /* getUserById code here */
-const getUserById = (userId) =>
-    fetch(`${baseUrl}/${userId}`).then((response) => response.json());
-console.log(getUserById(2));
+const getUserById = (userId) => {
+    return fetch(`${baseUrl}/${userId}`)
+        .then(response => response.json())
+        .then(result => console.log(result))
+}
+// getUserById();
 
 /* createUser code here */
-const createUser = (user) =>
-    fetch(baseUrl, {
-        method: "POST",
+const createUser = data => {
+    return fetch(baseUrl, {
+        method: 'POST',
         headers: {
-            "Content-Type": "application/json;charser= utf-8",
+            'Content-Type': 'application/json;charset=utf-8'
         },
-        body: JSON.stringify(user),
-    });
-
+        body: JSON.stringify(data)
+    })
+};
+// createUser();
 
 /* updateUser code here */
-const updateUser = (id, updatedUserData) =>
-    fetch(`${baseUrl}/${id}`, {
-        method: "PUT",
+const updateUser = (userId, data) => {
+    return fetch(`${baseUrl}/${userId}`, {
+        method: 'PUT',
         headers: {
-            "Content-Type": "application/json;charser= utf-8",
+            'Content-Type': 'application/json;charset=utf-8'
         },
-        body: JSON.stringify(updatedUserData),
-    });
-
+        body: JSON.stringify(data)
+    })
+};
 
 /* deleteUser code here */
-const deleteUser = (id) =>
-    fetch(`${baseUrl}/${id}`, {
-        method: "DELETE",
-    });
-export { getUsersList, getUserById, createUser, updateUser, deleteUser }
+const deleteUser = (userId) => {
+    return fetch(`${baseUrl}/${userId}`, {
+        method: 'DELETE',
+    })
+};
+
+export { getUsersList, getUserById, createUser, updateUser, deleteUser };
