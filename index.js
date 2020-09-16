4b238744c11560d8d70b31e6cfea0915d461e9f4
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-16 16:21:58 +0300

    js

--- index.js
+++ index.js
@@ -1,50 +1,27 @@
-//1. querySelector elements
-//2. считать данные формы
-//3. отправка через POST и обработать ответ (handle response)
-//4. валидация инпута
-//
-"use strict";
+window.addEventListener("error", function onUnhandledError (err) {
+    console.error("error", err.message);
+});
 
-const serverUrl = "https://5f60907d90cf8d0016557f3a.mockapi.io/api/v1/users";
+let userParsingResult = '{"name": "Ali"}';
 
-const loginForm = document.querySelector(".login-form");
-const submit = document.querySelector(".submit-button");
-const error = document.querySelector(".error-text");
+try {
+    const user = JSON.parse(userParsingResult);
+    console.log("User data: ", user);
+    userParsingResult = "success";
+} catch(e) {
+    userParsingResult = "error";
+} finally {
+    console.log(`User parsing finished with ${userParsingResult}`);
+}
 
+let productParsingResult = '{"name": "Leo"';
 
-// let userBody = {
-//   email: "apple@example.com",
-//   name: "Apple",
-// };
-
-// console.log(res);
-
-const form = () => {
-    if (loginForm.reportValidity()) {
-        submit.disabled = false;
-    }
-};
-loginForm.addEventListener("input", form);
-
-const sendData = (event) => {
-    event.preventDefault();
-    const userBody = Object.fromEntries([...new FormData(loginForm)]);
-
-    fetch(serverUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json;charser= utf-8",
-            },
-            body: JSON.stringify(userBody),
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            loginForm.reset();
-            alert(JSON.stringify(data));
-        })
-        .catch(() => {
-            error.textContent = "Failed to create user";
-        });
-};
-
-loginForm.addEventListener("submit", sendData);
+try {
+    const product = JSON.parse(productParsingResult);
+    console.log("Product data: ", product);
+    productParsingResult = "success";
+} catch(e) {
+    productParsingResult = "error";
+} finally {
+    console.log(`Product parsing finished with ${productParsingResult}`);
+} 
