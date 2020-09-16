896ba281cdd3e717bf2546a736eb1ebd544f51df
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-16 18:55:22 +0300

    js

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-window.addEventListener("error", function onUnhandledError (err) {
-    console.error("error", err.message);
-});
+const successRequest = Promise.resolve({ name: "Tom" });
 
-let userParsingResult = '{"name": "Ali"}';
+successRequest
+    .then(function onSuccess1(data) {
+        // console.log(data);
+        throw new Error('Error with data');
+    })
+    .catch(function onError1(error) {
+        console.error("onError1", error.message);
+    })
 
-try {
-    const user = JSON.parse(userParsingResult);
-    console.log("User data: ", user);
-    userParsingResult = "success";
-} catch(e) {
-    userParsingResult = "error";
-} finally {
-    console.log(`User parsing finished with ${userParsingResult}`);
-}
 
-let productParsingResult = '{"name": "Leo"';
+const failRequest = Promise.reject(new Error("Something went wrong"));
 
-try {
-    const product = JSON.parse(productParsingResult);
-    console.log("Product data: ", product);
-    productParsingResult = "success";
-} catch(e) {
-    productParsingResult = "error";
-} finally {
-    console.log(`Product parsing finished with ${productParsingResult}`);
-} 
+failRequest
+    .catch(function onError2(error) {
+        // console.error("onError2", error.message);
+        throw new Error('Server error');
+    })
+    .then(function onSuccess2(data) {
+        console.log("onSuccess2", data);
+    })
+    .catch(function onError3(error) {
+        console.error("onError3", error.message);
+    });
