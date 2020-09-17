b34263f35c3b7facd4b8bdc632943d8a28c2511e
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-17 15:21:01 +0300

    js

--- index.js
+++ index.js
@@ -1,37 +1,13 @@
-import { fetchUserData, fetchRepositories } from './gateways.js';
-import { renderUserData } from './user.js';
-import { renderRepos, cleanReposList } from './repos.js';
-import { showSpinner, hideSpinner } from './spinner.js';
+const strJson = '{"name": "Jack"}';
+// const a = JSON.parse(strJson);
 
-const defaultUser = {
-    avatar_url: 'https://avatars3.githubusercontent.com/u10001',
-    name: '',
-    location: '',
-};
+export const parseUser = str => {
+    try {
+        return JSON.parse(str)
+    }
+    catch {
+        return null;
+    }
+}
 
-renderUserData(defaultUser)
-
-const showUserBtnElem = document.querySelector('.name-form__btn');
-const userNameInputElem = document.querySelector('.name-form__input');
-
-const onSearchUser = () => {
-    showSpinner();
-    cleanReposList();
-    const userName = userNameInputElem.value;
-    fetchUserData(userName)
-        .then(userData => {
-            renderUserData(userData);
-            return userData.repos_url;
-        })
-        .then(url => fetchRepositories(url))
-        .then(reposList => {
-            renderRepos(reposList);
-            hideSpinner();
-        })
-        .catch(err => {
-            hideSpinner();
-            alert(err.message);
-        });
-};
-
-showUserBtnElem.addEventListener('click', onSearchUser);
+console.log(parseUser(strJson));
