28e7b9aa6922285699c286fd3bdf6e8a95524a08
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-23 16:49:32 +0300

    js

--- polyfills/array-flat.js
+++
@@ -1,9 +0,0 @@
-// file path: /polyfills/array-flat.js
-
-// здесь никакие экспорты не нужны
-
-console.log('polyfill for flat');
-
-Array.prototype.flat = Array.prototype.flat || function flat() {
-    // ... implementation for older browsers
-};
